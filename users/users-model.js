f468d3c00a6ac836c0c0702400c5be7eed46ce1e
Samuel Rowan <samuel@samuelrowan.com>
2020-05-11 03:41:23 -0500

    hash password

--- users/users-model.js
+++ users/users-model.js
@@ -1,6 +1,8 @@
+const bcrypt = require("bcryptjs")
 const db = require("../database/config")
 
 async function add(user) {
+	user.password = await bcrypt.hash(user.password, 14)
 	const [id] = await db("users").insert(user)
 	return findById(id)
 }
